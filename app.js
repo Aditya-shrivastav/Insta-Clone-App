693f081c59ba9016f1c277896974a252785ca62d
Aditya <adityashrivastava992001@gmail.com>
2021-02-02 00:53:27 +0530

    Changed storage location 4

--- app.js
+++ app.js
@@ -50,6 +50,9 @@ app.use("/profile",profileRouter);
 
 app.use(passport.session());
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/Client/build/index.html'))
+})
 
 
 app.listen(PORT,()=>{
